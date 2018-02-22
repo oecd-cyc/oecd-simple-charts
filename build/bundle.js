0ab5ad075e46a73be3ec0c1313aa252b39263648
moklick <info@moritzklack.com>
2018-02-22 11:03:02 +0100

    1.0.2

--- build/bundle.js
+++ build/bundle.js
@@ -1,4 +1,3 @@
-document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
